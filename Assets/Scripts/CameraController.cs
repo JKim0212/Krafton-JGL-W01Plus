11c0303c0c3b04f0338e185afc170a366a788836
Jonathan Kim <97498549+JKim0212@users.noreply.github.com>
2025-03-07 11:40:24 +0900

    player movement implemented

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,46 +2,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform player;
-    //private Transform playerTransform;
-
-    public bool inDamange;
-    private Vector3 damangeFXPosition;
-
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Assign player transform
-        //playerTransform = player.GetComponent<Transform>();
-
-        transform.position = new Vector3(player.position.x, player.position.y, -10);
+        
     }
 
-
+    // Update is called once per frame
     void Update()
     {
-        if(inDamange){
-            damangeFXPosition = new Vector3(MakeDamageFXMagnitude(), MakeDamageFXMagnitude(), 0);
-        }
-        else{
-            damangeFXPosition = Vector3.zero;
-        }
-
-        transform.position = new Vector3(player.position.x, player.position.y, -10) + damangeFXPosition;
-    }
-
-    float MakeDamageFXMagnitude(){
-        return Random.Range(-0.2f, 0.2f);
-    }
-
-    // Is only called when player got damage in positive value(damaging)
-    public void ShakeCamera(){
-        inDamange = true;
-        Invoke(nameof(StopShake), 0.1f);
-    }
-
-    private void StopShake()
-    {
-        inDamange = false;
+        
     }
 }
